e31d6777a3190486406d64a3950d394dc2274ee4
Ahammed Almas <ahmdalmasbsns@gmail.com>
2025-01-05 16:38:26 +0530

    new

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the current directory
-app.use(express.static(path.join(__dirname)));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public'), {
+    maxAge: '1d' // Cache static assets for 1 day
+}));
 
+// Serve static files from the current directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
